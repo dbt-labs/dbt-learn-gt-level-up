b0668be698e35f6efbdfec72a393896fec367abd
David Opoku <david.opoku@dbtlabs.com>
2024-08-28 10:52:57 +0000

    Change source CTE to source2 in stg_jaffle_shop__orders model

--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -1,6 +1,6 @@
 with 
 
-source as (
+source2 as (
 
     select * from {{ source('jaffle_shop', 'orders') }}
 
@@ -10,13 +10,14 @@ renamed as (
 
     select
     -- adding a comment to demo CI job
+
         id as order_id,
         user_id as customer_id,
         order_date,
         status,
         _etl_loaded_at
 
-    from source
+    from source2
 
 )
 
