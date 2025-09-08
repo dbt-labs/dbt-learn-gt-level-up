044e5a252b9b7c32aeafc11a1d7a48f9c6f1372a
Anushae Syed <anushae.syed3@dbtlabsworkshop.com>
2025-09-08 07:39:48 +0000

    checked demos and hands-on for incrementals and advanced testing

---
+++ tests/generic/assert_date_greater_2017.sql
@@ -0,0 +1,8 @@
+{% test assert_date_greater_2017(model, column_name) %}
+
+select
+    {{ column_name }} 
+from {{ model }}
+where {{ column_name }} < '2018-01-01'
+
+{% endtest %}
---
+++ tests/payment_amount_greater_than_five.sql
@@ -0,0 +1,5 @@
+
+select
+   amount_usd
+from {{ ref('stg_stripe__payment') }}
+where amount_usd <= 5
