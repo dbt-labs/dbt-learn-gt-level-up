ef17721ec487297c7ad013014ca22f86dc99e461
Anushae Syed <anushae.syed3@dbtlabsworkshop.com>
2025-09-11 08:27:10 +0000

    PMI day 1 and 2

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -8,7 +8,7 @@ orders as (
 
 customer_orders as (
     select
-        customer_id,
+        customer_id, 
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders,
