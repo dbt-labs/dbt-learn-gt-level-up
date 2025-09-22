4aa227cd3ede5fb3cd9ef9b69fc8e33b5c612359
Anushka Sah <anushka.sah@dbtlabsworkshop.com>
2025-09-22 19:46:01 +0000

    Testing the commit

---
+++ models/intermediate/int_popular_products.sql
@@ -0,0 +1,19 @@
+-- models/intermediate/int_popular_products.sql
+with 
+
+order_items as (
+    select * from {{ ref('stg_jaffle_shop__order_items') }}
+),
+
+
+product_count as (
+    select
+        product_id,
+        product_name,
+        count(order_item_id) as purchase_count
+    from order_items
+    group by product_id, product_name
+    order by purchase_count desc
+)
+
+select * from product_count
