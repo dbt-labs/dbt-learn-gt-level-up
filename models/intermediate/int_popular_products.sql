d926a3c69918bee83a26b1d363492906d705add4
Tim Trombetta <tim.trombetta@dbtlabsworkshop.com>
2025-09-22 20:33:43 +0000

    made changes

---
+++ models/intermediate/int_popular_products.sql
@@ -0,0 +1,24 @@
+-- models/intermediate/int_popular_products.sql
+with order_items as (
+
+    select * from {{ ref('stg_jaffle_shop__order_items') }}
+
+),
+
+item_order_count as (
+
+    select
+        product_id,
+        product_name,
+        count(order_item_id) as order_count
+    from order_items
+    group by product_id, product_name
+
+
+)
+select
+    product_id,
+    product_name,
+    order_count
+from item_order_count
+order by order_count desc
