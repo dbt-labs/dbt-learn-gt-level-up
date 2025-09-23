55c574affeb8bc9ca99e5a72de959bd5b6668fdb
Michal Wyszomirski <michal.wyszomirski@dbtlabsworkshop.com>
2025-09-23 08:58:27 +0000

    added incremental model

--- models/marts/fct_page_views.sql
+++ models/marts/fct_page_views.sql
@@ -1,12 +1,17 @@
 {{
     config(
-        tags='nightly',
-        materialized='incremental'
+        materialized='incremental',
+        unique_key='page_view_id'
     )
 }}
-
+ 
 with events as (
     select * from {{ ref('stg_snowplow__events') }}
+    {% if is_incremental() %}
+        -- this filter will only be applied on an incremental run
+        where collector_tstamp > (select dateadd ('day', -3, max(max_collector_tstamp)) from {{ this }})
+    {% endif %}
+ 
 ),
 page_views as (
     select * from events
@@ -28,3 +33,4 @@ joined as (
     left join aggregated_page_events using (page_view_id)
 )
 select * from joined
+ 
