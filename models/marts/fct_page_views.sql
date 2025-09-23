df1108b3abb120ab03cd4400856c035e5041d6e7
Anushae Syed <anushae.syed3@dbtlabsworkshop.com>
2025-09-23 08:58:15 +0000

    added incremental model

--- models/marts/fct_page_views.sql
+++ models/marts/fct_page_views.sql
@@ -1,11 +1,17 @@
 {{
     config(
-        tags='nightly'
+        materialized='incremental',
+        unique_key='page_view_id'
     )
 }}
 
 with events as (
     select * from {{ ref('stg_snowplow__events') }}
+    {% if is_incremental() %}
+        -- this filter will only be applied on an incremental run
+        where collector_tstamp > (select dateadd ('day', -3, max(max_collector_tstamp)) from {{ this }}) 
+    {% endif %}
+
 ),
 page_views as (
     select * from events
