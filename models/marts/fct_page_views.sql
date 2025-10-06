dd84046f47514c110785037cb5f89c4390bd3888
vinay koppolu <vinay.koppolu+1759759970@dbtlabsworkshop.com>
2025-10-06 15:59:52 +0000

    commit

--- models/marts/fct_page_views.sql
+++ models/marts/fct_page_views.sql
@@ -1,11 +1,16 @@
 {{
     config(
+        materialized='table',
         tags='nightly'
     )
 }}
 
 with events as (
     select * from {{ ref('stg_snowplow__events') }}
+    {% if is_incremental()%}
+       -- this filter will only be applied on an incrementak run
+       where collector_tstamp > (select max(max_collector_tstamp)) from {{ this }}
+    {% endif %}
 ),
 page_views as (
     select * from events
