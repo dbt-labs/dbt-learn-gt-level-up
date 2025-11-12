b72823157000ee96867ae83a6001caea0e634b84
deepa zodge <deepa.zodge@dbtlabsworkshop.com>
2025-11-12 17:35:45 +0000

    Updated yml files and added tests

---
+++ tests/generic/not_null.sql
@@ -0,0 +1,26 @@
+{% test not_null_or_empty_string(model, column_name) %}
+
+with validation as (
+
+    select
+        {{ column_name }} as field_value
+
+    from {{ model }}
+
+),
+
+validation_errors as (
+
+    select
+        field_value
+
+    from validation
+    where field_value is null
+    and field_value = ''
+
+)
+
+select *
+from validation_errors
+
+{% endtest %}
